108b591f2d80682c2bf28a08c0fd19ab8607e9cf
Ismaele <tiados04@gmail.com>
2025-07-12 18:51:40 +0200

    feat: added serial values information

--- lidar/lidarLib.py
+++ lidar/lidarLib.py
@@ -1,36 +1,15 @@
-from rplidar import RPLidar
-import matplotlib.pyplot as plt
-import numpy as np
-import matplotlib.animation as animation
+import serial
+import struct
+import time
 
-PORT_NAME = '/dev/ttyUSB0'
-DMAX = 4000
-IMIN = 0
-IMAX = 50
 
-def update_line(num, iterator, line):
-    scan = next(iterator)
-    offsets = np.array([(np.radians(meas[1]), meas[2]) for meas in scan])
-    line.set_offsets(offsets)
-    intens = np.array([meas[0] for meas in scan])
-    line.set_array(intens)
-    return line,
+class Lidar:
+    SYNC_A = 0xA5
+    SYNC_B = 0x5A
 
-def run():
-    lidar = RPLidar(PORT_NAME)
-    fig = plt.figure()
-    ax = plt.subplot(111, projection='polar')
-    line = ax.scatter([0, 0], [0, 0], s=5, c=[IMIN, IMAX],
-                           cmap=plt.cm.Greys_r, lw=0)
-    ax.set_rmax(DMAX)
-    ax.grid(True)
+    CMD_GET_INFO   = 0x50
+    CMD_GET_HEALTH = 0x52
+    CMD_START      = 0x20
+    CMD_STOP       = 0x25
 
-    iterator = lidar.iter_scans()
-    ani = animation.FuncAnimation(fig, update_line,
-        fargs=(iterator, line), interval=50)
-    plt.show()
-    lidar.stop()
-    lidar.disconnect()
-
-if __name__ == '__main__':
-    run()
+    DESCRIPTOR_LEN = 7
