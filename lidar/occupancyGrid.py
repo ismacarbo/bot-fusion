77667b570f3b8cd3014430481d658b7a05950b16
Ismaele <tiados04@gmail.com>
2025-07-15 20:36:48 +0200

    feat: added bresenham algorithm and world to map translation"

--- lidar/occupancyGrid.py
+++ lidar/occupancyGrid.py
@@ -8,3 +8,35 @@ class OccupancyGrid:
         self.xMin,self.yMin=xMin,yMin
         self.log_odds=np.zeros((self.height,self.width),dtype=float)
         pass
+
+    def world_to_map(self, x, y):
+        j = int((x - self.x_min) / self.resolution)
+        i = int((y - self.y_min) / self.resolution)
+        return i, j
+
+    def map_to_world(self, i, j):
+        x = self.x_min + (j + 0.5) * self.resolution
+        y = self.y_min + (i + 0.5) * self.resolution
+        return x, y
+    
+    def bresenhamLine(i0, j0, i1, j1):
+        cells = []
+        di = abs(i1 - i0)
+        dj = abs(j1 - j0)
+        si = 1 if i0 < i1 else -1
+        sj = 1 if j0 < j1 else -1
+        err = di - dj
+        i, j = i0, j0
+
+        while True:
+            cells.append((i,j))
+            if i == i1 and j == j1:
+                break
+            e2 = 2*err
+            if e2 > -dj:
+                err -= dj
+                i += si
+            if e2 < di:
+                err += di
+                j += sj
+        return cells
