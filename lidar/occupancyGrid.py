76ef461e11fcac6a5f13f129e7b7823bf0957937
Ismaele <tiados04@gmail.com>
2025-07-15 20:28:16 +0200

    feat: started occupancy grid class

--- lidar/occupancyGrid.py
+++ lidar/occupancyGrid.py
@@ -1,26 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt 
 
-gridSize=(20,20)
-occupancyGrid=np.full(gridSize,-1) #-1 unknown, 0 free, 1 occupied
-
-obstacles=[(np.random.randint(0,gridSize[0]),np.random.randint(0,gridSize[1])) for i in range(10)]
-
-for x,y in obstacles:
-    occupancyGrid[x][y]=1 
-
-    
-freeCells=[(x,14) for x in range(5,15)]
-
-for x,y in freeCells:
-    occupancyGrid[y,x]=0
-
-fig, ax = plt.subplots(figsize=(6, 6))
-cmap=plt.cm.get_cmap('grey',3)
-cmap.set_bad(color='black')
-ax.imshow(occupancyGrid,cmap=cmap,origin='lower')
-ax.set_title("Occupancy Grid")
-ax.set_xlabel("X")
-ax.set_ylabel("Y")
-plt.grid(True)
-plt.show()
+class OccupancyGrid:
+    def __init__(self,xMin,xMax,yMin,yMax,resolution):
+        self.resolution=resolution
+        self.width=int(np.ceil((xMax-yMin)/resolution))
+        self.height=int(np.ceil((yMax-yMin)/resolution))
+        self.xMin,self.yMin=xMin,yMin
+        self.log_odds=np.zeros((self.height,self.width),dtype=float)
+        pass
