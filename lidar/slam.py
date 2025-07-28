dba87c65e4184d55fb47c685f0b3eee12a8e38c5
Ismaele <tiados04@gmail.com>
2025-07-28 02:19:44 +0200

    feat: added odometry simulation

--- lidar/slam.py
+++ lidar/slam.py
@@ -12,7 +12,7 @@ class SLAM2D:
         #intial factors
         self.graph=gtsam.NonlinearFactorGraph()
         self.initial=gtsam.Values()
-        
+
         sigmas=np.array(sigma_odom)
         self.noiseOdom=gtsam.noiseModel.Diagonal.Sigmas(sigmas)
 
@@ -25,3 +25,20 @@ class SLAM2D:
         self.initial.insert(0, gtsam.Pose2(0,0,0))
         self.last_pose = gtsam.Pose2(0,0,0)
         self.counter   = 0
+
+    def addOdom(self,T4x4):
+        i=self.counter+1
+        dx=T4x4[0,3]
+        dy=T4x4[1,3]
+        theta=np.arctan2(T4x4[1,0],T4x4[0,0])
+
+        #creating the odometry factor
+        odo=gtsam.Pose2(dx,dy,theta) #measurement between i-1 and i (two nodes)
+        self.graph.add(gtsam.BetweenFactorPose2(i-1,i,
+                                        odo,
+                                        self.noiseOdom)) #add the factor to the graph for the optimization
+        #update the last pose
+        estimate=self.last_pose.compose(odo)
+        self.initial.insert(i,estimate)
+        self.last_pose=estimate
+        self.counter +=1
